901a3ad14b7a147472ff9354dec0f1462be4fe63
Ian Holmes <ihholmes@gmail.com>
2024-06-04 01:03:23 -0700

    6502

--- vm.js
+++ vm.js
@@ -3,12 +3,12 @@ import MersenneTwister from 'mersennetwister';
 
 class BoardMemory {
     constructor(seed = 42) {
-        this.data = new UIntArray (this.B * this.B * this.M);
+        this.data = new UIntArray (this.storageSize);
         this.mt = new MersenneTwister (seed);
-        this.randomizeCell();
+        this.sampleOriginAndDuration();
     }
 
-    randomizeCell() {
+    sampleOriginAndDuration() {
         const rv1 = mt.int();
         this.iOrig = rv1 & 0xFF;
         this.jOrig = (rv1 & 0xFF00) >> 8;
@@ -28,12 +28,17 @@ class BoardMemory {
 
     get B() { return 256 }
     get M() { return 1024 }
-    get HIMEM() { return 0x63FF }
+    get N() { return 5 }
+    get log2M() { return 10 }  // = log_2(M)
+    get storageSize() { return this.B * this.B * this.M; }
+    get neighborhoodSize() { return this.N * this.N * this.M; }
+    get cellOffsetMask() { return this.M - 1 }
+    get HIMEM() { return this.neighborhoodSize - 1 }
 
     wrapCoord (i) { return (i + this.B) % this.B; }
 
     getCell (idx) { return this.data[idx]; }
-    setCell (idx, val) { this.data[idx] = val; }
+    setCell (idx, val) { this.data[idx] = val & 0xFF; }
 
     ijbToCellIndex (i, j, b) {
         return this.M * (j + this.B * this.i) + b;
@@ -42,10 +47,10 @@ class BoardMemory {
     addrToCellIndex (addr) {
         if (addr < 0 || addr > this.HIMEM)
             return -1;
-        const b = addr & 0x3FF;
-        const nbrIdx = addr >> 10;
-        const x = Math.floor (nbrIdx / 5);
-        const y = nbrIdx % 5;
+        const b = addr & this.cellOffsetMask;
+        const nbrIdx = addr >> this.log2M;
+        const x = Math.floor (nbrIdx / this.N);
+        const y = nbrIdx % this.N;
         return this.ijbToCellIndex (this.wrapCoord (this.i + x),
                                     this.wrapCoord (this.j + y),
                                     b);
@@ -62,10 +67,77 @@ class BoardMemory {
             this.setCell (idx, val);
     }
 
-    readNeighborhood() { return new Uint8Array (Array.from({length: this.HIMEM + 1}).map((_,addr) => this.read(addr))) }
-    writeNeighborhood (snapshot) { snapshot.forEach ((val, addr) => this.write (addr, val)) }
+    readNeighborhood() {
+        return new Uint8Array (Array.from({length: this.HIMEM + 1})
+                                    .map((_,addr) => this.read(addr)));
+    }
+    writeNeighborhood (snapshot) {
+        snapshot.forEach ((val, addr) => this.write (addr, val));
+    }
 };
 
 class BoardController {
-    
+    constructor (board) {
+        this.board = board || new BoardMemory();
+        this.resetSfotty()
+        this.sfotty = new Sfotty(this.board);
+        this.cycleCounter = 0;
+    }
+
+    resetSfotty() {
+        this.sfotty = new Sfotty(this.memory);
+    }
+
+    get cyclesToNextInterrupt() {
+        return this.board.cycles;
+    }
+
+    randomizeLocation() {
+        this.board.sampleOriginAndDuration();
+        this.undoSnapshot = this.board.readNeighborhood();
+    }
+
+    undo() {
+        this.board.writeNeighborhood (this.undoSnapshot);
+    }
+
+    pushByte (val) {
+        const S = this.sfotty.S;
+        this.board.write (0x100 + S, val);
+        this.sfotty.S = S - 1;
+    }
+
+    pushWord (val) {
+        const hi = (val >> 8) & 0xFF, lo = val & 0xFF;
+        this.push (hi);
+        this.push (lo);
+    }
+
+    runToNextInterrupt() {
+        while (true) {
+            this.sfotty.run();
+            if (this.sfotty.cycleCounter >= this.cyclesToNextInterrupt) {
+                this.cycleCounter += this.cyclesToNextInterrupt;
+                if (this.sfotty.I)
+                    this.undo();
+                else {
+                    this.pushWord (sfotty.PC);
+                    this.pushByte (sfotty.P);
+                    this.board.write (0x0200, sfotty.S);
+                    this.board.write (0x0201, sfotty.A);
+                    this.board.write (0x0202, sfotty.X);
+                    this.board.write (0x0203, sfotty.Y);
+                }
+                this.randomizeLocation();
+                this.sfotty.S = 0;
+                this.sfotty.A = 0;
+                this.sfotty.X = 0;
+                this.sfotty.Y = 0;
+                this.sfotty.P = 0;
+                this.sfotty.PC = 0;
+                this.sfotty.cycleCounter = 0;
+                break;
+            }
+        }
+    }
 };
